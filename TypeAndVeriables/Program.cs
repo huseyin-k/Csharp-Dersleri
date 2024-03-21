97ae284ffc320f8377c3d1a37916b97b898e1fad
Hüseyin Kurt <kurth2517@gmail.com>
2024-03-21 03:18:01 +0300

    first commit

---
+++ TypeAndVeriables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeAndVeriables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            Console.WriteLine("number1 is {0}",number1);
+            //Console.WriteLine("Hello worl!");
+                     
+        }
+    }
+}
