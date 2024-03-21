0b480ba93393f5d8a768b625321068c7abc2b8f4
Hüseyin Kurt <kurth2517@gmail.com>
2024-03-22 01:28:12 +0300

    tree commit

--- TypeAndVeriables/Program.cs
+++ TypeAndVeriables/Program.cs
@@ -10,10 +10,12 @@ namespace TypeAndVeriables
     {
         static void Main(string[] args)
         {
-            int number1 = 10;
+            int number1 = -2147483648;
             Console.WriteLine("number1 is {0}",number1);
             Console.WriteLine("Hello worl!");
-                     
+            Console.WriteLine("Hello worl!"); 
+            Console.WriteLine("Hello worl!");
+
         }
     }
 }
