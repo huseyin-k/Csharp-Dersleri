e5c8b07eb5e145e8284d60159df7535b5af61d90
Hüseyin Kurt <kurth2517@gmail.com>
2024-03-22 02:46:04 +0300

    veri tipleri

--- TypeAndVeriables/Program.cs
+++ TypeAndVeriables/Program.cs
@@ -9,11 +9,13 @@ namespace TypeAndVeriables
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int number1 = -2147483648;
+        {   short number3 = -32768;
+            int number1 = 2147483647;
+            long number2 = 922337203685547757;
             Console.WriteLine("number1 is {0}",number1);
-            Console.WriteLine("Hello worl!");
-            
+            Console.WriteLine("number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+                
         }
     }
 }
